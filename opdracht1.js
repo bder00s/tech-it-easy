c53ebdfddfb8757a0c6e54b89cbcb8078c6f557e
bder00s <bonne-deroos@hotmail.com>
2022-06-30 17:20:50 +0200

    opdracht 1a gemaakt

---
+++ opdracht1.js
@@ -0,0 +1,18 @@
+//opdracht 1A
+
+//
+// function tvModels(arr) {
+//     return arr.map((entry) => {
+//         return entry.brand;
+//     });
+// }
+// const output = tvModels(inventory);
+// console.log(output);
+
+const newArray = inventory.map((entry) => {
+    return entry.brand
+})
+console.log(newArray);
+
+//opdracht 1B
+
