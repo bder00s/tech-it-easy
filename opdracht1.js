9f28140b5d2d16bea944da722351fcf381b67d01
bder00s <bonne-deroos@hotmail.com>
2022-07-03 19:40:22 +0200

    opdracht 1 en 2 gemaakt

--- opdracht1.js
+++ opdracht1.js
@@ -16,3 +16,31 @@ console.log(newArray);
 
 //opdracht 1B
 
+const soldOut = inventory.filter((stock ) => {
+    return stock.originalStock <= stock.sold;
+});
+console.log(soldOut);
+
+
+//opdracht 1C
+
+const ambiTV = inventory.filter((gotAmbilight) => {
+    return gotAmbilight.options.ambiLight === true;
+});
+console.log(ambiTV);
+
+
+//opdracht 1D
+
+inventory.sort((a,b) => {
+    if (a.price < b.price) {
+        return -1;
+    }
+    if (a.price > b.price) {
+        return 1;
+    }
+    return 0;
+
+});
+console.log(inventory);
+
